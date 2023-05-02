39133503383d47aff2998cb46990a8ab6990ab4c
ранел <56401315+kurech@users.noreply.github.com>
2023-05-02 12:06:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function menuDropDown() {
+	document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Закрыть раскрывающийся список, если пользователь щелкнет за его пределами.
+window.onclick = function (event) {
+	if (!event.target.matches('.dropbtn')) {
+		var dropdowns = document.getElementsByClassName("dropdown-content");
+		var i;
+		for (i = 0; i < dropdowns.length; i++) {
+			var openDropdown = dropdowns[i];
+			if (openDropdown.classList.contains('show')) {
+				openDropdown.classList.remove('show');
+			}
+		}
+	}
+}
+
+function filterDropDown() {
+	document.getElementById("myDropdownFilter").classList.toggle("show");
+}
